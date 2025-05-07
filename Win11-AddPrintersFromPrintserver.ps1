0b9b23ab7eb56dff47eefc1af916b99e717c1f7b
zolabus <alezola@gmail.com>
2025-05-07 16:44:35 +0200

    Add files via upload

---
+++ Win11-AddPrintersFromPrintserver.ps1
@@ -0,0 +1,32 @@
+<#
+  .NOTES
+  ===========================================================================
+   Created on:   	23.06.2022
+   Created by:   	Simon Skotheimsvik
+   Filename:     	Win11-AddPrintersFromPrintserver.ps1
+   Instructions:    https://skotheimsvik.blogspot.com/2022/06/install-printers-from-ad-printserver-on.html
+  ===========================================================================
+  
+  .DESCRIPTION
+    This script will connect printers from a printserver to an Azure AD joined
+    Windows11 device with user signed in with a hybrid identity.
+    
+  .EXAMPLE
+    Win11-AddPrintersFromPrintserver.ps1 
+#>
+
+# $Printers = (Get-Printer -ComputerName PrintServer).Name
+# $Printers = (Get-Printer -ComputerName PrintServer | Where-Object {$_.Name -like "Simon*"}).Name
+$printers = @(
+  '\\printserver\printer1'
+  '\\printserver\printer2'
+  '\\printserver\printer3'
+  '\\printserver\printer4'
+)
+
+ForEach ($printer in $printers) {
+  $IsInstalled = [bool](Get-Printer | Where-Object { $_.Name -eq $printer })
+  if (-not $IsInstalled) {
+    Add-Printer -ConnectionName $printer -ErrorAction Stop
+  }
+}
